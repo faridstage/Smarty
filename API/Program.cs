9715a804c64009365d965acd598ded8ac9ced26d
farid <faridfaiz71@gmail.com>
2023-05-29 22:03:51 +0300

    End of Section 16 (validation)

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 
 var app = builder.Build();
 
@@ -21,10 +22,8 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
+app.UseSwaggerDocumentation();
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseStaticFiles();
 
