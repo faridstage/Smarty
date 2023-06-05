6af1b952e5e9ff9ab51006efb8dc99f1225f3ecd
ezz al din essam <eessam@stingrayconsultation.com>
2023-06-06 02:08:09 +0300

    front first task

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Smarty';
+export class AppComponent implements OnInit {
+  title = 'client';
+  products : any[] = [];
+  router:any;
+  constructor(private http:HttpClient,private route: ActivatedRoute){
+    this.router = route;
+    console.log(route)
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: response => console.log(response),
+      error: error => console.error(error),
+      complete: ()=>{
+        console.log('request completed');
+        console.log('extra statments');
+      }
+      
+    })
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProgramsComponent,
+    SubjectsComponent,
+    NotFoundComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CarouselModule
   ],
-  providers: [],
+  providers: [
+    { provide: Window, useValue: window }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
